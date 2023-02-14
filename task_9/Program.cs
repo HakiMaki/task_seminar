24e2e26ca78c94ed81456750d8b3374316fe1058
Khakim_Azisov <khakimazisov@gmail.com>
2023-02-14 17:43:12 +0800

    Работали на семинаре

---
+++ task_9/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Clear();
+Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num2 = int.Parse(Console.ReadLine());
+if ((num * num == num2 )
+   || (num2 * num2 == num))
+{
+Console.WriteLine("Второе квадрат Первого и наоборот");
+}
+
